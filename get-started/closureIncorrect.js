5464243ff2fd996d18f66c614c6b2646deb2a521
ZH Zhang <zhangzhhz@gmail.com>
2020-02-15 11:41:17 +0000

    add more comments on how it is incorrect

--- get-started/closureIncorrect.js
+++ get-started/closureIncorrect.js
@@ -1,18 +1,30 @@
 "use strict";
 
+/* Lesson learned:
+   Do not update an enclosed vairable directly.
+   Use a locally defined variable inside the closure function, 
+   or pass the eclosed variable to another function (inside the closure function)
+   as an argument.
+*/
 
 function range(start,end) {
+  console.log(`start = ${start}`);
   let s = start; 
   /* Having a variable here outside of function `rangeTo` 
      or directly using `start` in function `rangeTo` 
-     will make the last 2 lines print incorrect value.
+     will make the last 2 lines print incorrect value
+     when using the while loop which updates `s` or `start` directly.
       start3(3);     // [3]
       start3(8);     // [3,4,5,6,7,8], actually printed [4,5,6,7,8]
-      start3(10);     // [3,4,5,6,7,8,9,10], actually printed [9,10]
-     It appears that 
+      start3(10);    // [3,4,5,6,7,8,9,10], actually printed [9,10]
+     The same `s` is used across different calls to `rangeTo()` 
+     that is returned to variable `start3`.
   */
   function rangeTo(end) {
-    // let s = start; // Using a locally defined variable here to store the value of `start` will fix the issue.
+    // let s = start;
+    // Using a locally defined variable `s` here to store the value of 
+    // `start` will fix the issue.
+    console.log(`s = ${s}`);
     let arr = [];
     /*
     for (let i = start; i <= end; i++) {
@@ -30,7 +42,7 @@ function range(start,end) {
     
     console.log(arr);
   }
-
+  
   if (end === undefined) {
     return rangeTo;
   }
@@ -39,16 +51,20 @@ function range(start,end) {
   }
 }
 
-range(3,3);    // [3]
-range(3,8);    // [3,4,5,6,7,8]
-range(3,0);    // []
+// range(3,3);    // [3]
+// range(3,8);    // [3,4,5,6,7,8]
+// range(3,0);    // []
 
 var start3 = range(3);
-var start4 = range(4);
+// var start4 = range(4);
 
+console.log("***** start3(3) *****");
 start3(3);     // [3]
+console.log("***** start3(8) *****");
 start3(8);     // [3,4,5,6,7,8]
+console.log("***** start3(10) *****");
 start3(10);     // [3,4,5,6,7,8, 9, 10]
+console.log("***** start3(0) *****");
 start3(0);     // []
 
-start4(6);     // [4,5,6]
+// start4(6);     // [4,5,6]
